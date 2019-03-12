90664914e11854e5aa86176de865fd8c5e7e03be
DaanVerkooij <d@anverkooij.nl>
2019-03-12 15:27:50 +0100

    str/float fix

--- datareader.py
+++ datareader.py
@@ -70,35 +70,40 @@ def importExcelData(f):
     # Process defines wheter or not data is completed and converted to SI \
     # or standard units, default is false.
     
-    arr=np.genfromtxt(f,delimiter=',',dtype='str')
+    arr_str=np.genfromtxt(f,delimiter=',',dtype='str')
+    
+    arr=np.genfromtxt(f,delimiter=',',dtype='float')
     
     #y,x
-    date_of_flight=arr[2,3] 
-    flight_number=arr[3,3] 
-    TO_time=arr[2,5]
-    LND_time=arr[3,5]
+    date_of_flight=arr_str[2,3] 
+    flight_number=arr_str[3,3] 
+    TO_time=arr_str[2,5]
+    LND_time=arr_str[3,5]
     #order = [p1,p2,coord,1L,1R,2L,2R,3L,3R]
     passengerMass=arr[7:16,7] #in kg
-    passengerNames=arr[7:16,3]
-    passengerPos=arr[7:16,0]
+    passengerNames=arr_str[7:16,3]
+    passengerPos=arr_str[7:16,0]
     
     
     blockfuel=arr[17,3] #in lbs
     #Aircraft config
-    ACC_CLCD=arr[22,4]
+    ACC_CLCD=arr_str[22,4]
     
     #time [min:sec], Elapsed time [sec] #empty, hp [ft] (pressure altitude)\
     #IAS [kts], a [deg], FFL [lbs/hr], FFr [lbs/hr], F. used [lbs], TAT #C
     CL_CD_series1=arr[27:33,1:9]
+    CL_CD_series1str=arr_str[27:33,1:9]
     CL_CD_series2=arr[43:49,1:9]
+#    CL_CD_series2str=arr_str[43:49,1:9]
+    
     
     #fill in ET
     for i in range(len(CL_CD_series1[:,0])):
-        time=convertToSec(CL_CD_series1[i,0])
-        CL_CD_series1[i,1]=time
+        time=convertToSec(CL_CD_series1str[i,0])
+        CL_CD_series1[i,1]=int(time)
         
     #Aircraft config
-    ACC_Trim=arr[53,4]
+    ACC_Trim=arr_str[53,4]
     
     #time [hrs:min], Elapsed time [sec] #empty, hp [ft] (pressure altitude)\
     #IAS [kts], a [deg], de [deg], detr [deg], Fe [N], FFL [lbs/hr], \ 
@@ -107,9 +112,9 @@ def importExcelData(f):
     
     #CG shift: 
     #_shifted all relates to moved person
-    name_shifted=arr[69,1]
-    pos_shifted=arr[69,1]
-    newpos_shifted=[70,4]
+    name_shifted=arr_str[69,1]
+    pos_shifted=arr_str[69,1]
+    newpos_shifted=arr_str[70,4]
     
     #time [hrs:min], Elapsed time [sec] #empty, hp [ft] (pressure altitude)\
     #IAS [kts], a [deg], de [deg], detr [deg], Fe [N], FFL [lbs/hr], \ 
@@ -118,66 +123,13 @@ def importExcelData(f):
     
     #Eigenmotions
     eigenmotions=[]
-    phugoid=arr[82,3]
-    shortPeriod=arr[83,3]
-    dutchRoll=arr[82,6]
-    dutchRollYD=arr[83,6]
-    aperRoll=arr[82,9]
-    spiral=arr[83,9]
+    phugoid=arr_str[82,3]
+    shortPeriod=arr_str[83,3]
+    dutchRoll=arr_str[82,6]
+    dutchRollYD=arr_str[83,6]
+    aperRoll=arr_str[82,9]
+    spiral=arr_str[83,9]
     eigenmotions.extend((phugoid,shortPeriod,dutchRoll,dutchRollYD,aperRoll,spiral))
-
-    # y,x
-    date_of_flight = arr[2, 3]
-    flight_number = arr[3, 3]
-    TO_time = arr[2, 5]
-    LND_time = arr[3, 5]
-    # order = [p1,p2,coord,1L,1R,2L,2R,3L,3R]
-    passengerMass = arr[7:16, 7]  # in kg
-    passengerNames = arr[7:16, 3]
-    passengerPos = arr[7:16, 0]
-
-    blockfuel = arr[17, 3]  # in lbs
-    # Aircraft config
-    ACC_CLCD = arr[22, 4]
-
-    # time [min:sec], Elapsed time [sec] #empty, hp [ft] (pressure altitude)\
-    # IAS [kts], a [deg], FFL [lbs/hr], FFr [lbs/hr], F. used [lbs], TAT #C
-    CL_CD_series1 = arr[27:33, 1:9]
-    CL_CD_series2 = arr[43:49, 1:9]
-
-    # fill in ET
-    for l in (CL_CD_series1, CL_CD_series2):
-        for i in range(len(l[:, 0])):
-            time = CL_CD_series1[i, 0]
-    #            print time
-    # Aircraft config
-    ACC_Trim = arr[53, 4]
-
-    # time [hrs:min], Elapsed time [sec] #empty, hp [ft] (pressure altitude)\
-    # IAS [kts], a [deg], de [deg], detr [deg], Fe [N], FFL [lbs/hr], \
-    # FFr [lbs/hr], F. used [lbs], TAT #C
-    El_Trim_Curve = arr[58:64, 1:12]
-
-    # CG shift:
-    # _shifted all relates to moved person
-    name_shifted = arr[69, 1]
-    pos_shifted = arr[69, 1]
-    newpos_shifted = [70, 4]
-
-    # time [hrs:min], Elapsed time [sec] #empty, hp [ft] (pressure altitude)\
-    # IAS [kts], a [deg], de [deg], detr [deg], Fe [N], FFL [lbs/hr], \
-    # FFr [lbs/hr], F. used [lbs], TAT #C
-    Cg_shift = arr[74:75, 1:12]
-
-    # Eigenmotions
-    eigenmotions = []
-    phugoid = arr[82, 3]
-    shortPeriod = arr[83, 3]
-    dutchRoll = arr[82, 6]
-    dutchRollYD = arr[83, 6]
-    aperRoll = arr[82, 9]
-    spiral = arr[83, 9]
-    eigenmotions.extend((phugoid, shortPeriod, dutchRoll, dutchRollYD, aperRoll, spiral))
 
     return date_of_flight, flight_number, TO_time, LND_time, passengerMass, \
            passengerNames, passengerPos, blockfuel, ACC_CLCD, CL_CD_series1, CL_CD_series2, \
@@ -214,11 +166,11 @@ def getFDValues(f):
 #%%
 #    Test functions
 #    
-#f='Reference_Datasheet.csv'
-#date_of_flight, flight_number, TO_time, LND_time, passengerMass, passengerNames\
-#, passengerPos, blockfuel, ACC_CLCD, CL_CD_series1, CL_CD_series2, ACC_Trim,\
-# El_Trim_Curve, name_shifted, pos_shifted, newpos_shifted, Cg_shift, eigenmotions\
-# = importExcelData(f)
+f='Reference_Datasheet.csv'
+date_of_flight, flight_number, TO_time, LND_time, passengerMass, passengerNames\
+, passengerPos, blockfuel, ACC_CLCD, CL_CD_series1, CL_CD_series2, ACC_Trim,\
+ El_Trim_Curve, name_shifted, pos_shifted, newpos_shifted, Cg_shift, eigenmotions\
+ = importExcelData(f)
 # 
 #print eigenmotions[0]
 #print convertToSec(eigenmotions[0])
