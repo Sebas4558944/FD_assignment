0fa520772c91adffa43f06d2ac6793dbc1373306
Martijn <ms.jorissen@tiscali.nl>
2019-03-20 10:57:02 +0100

    asym check

--- simplifiedEOM_eigenvalues.py
+++ simplifiedEOM_eigenvalues.py
@@ -90,10 +90,10 @@ def sym():
 
 
 def asym():
-    A=16*mub**3*(KX2*KZ2-KXZ)
-    B=-4*mub**2*(2*CYb*(KX2*KZ2-KXZ)+Cnr*KX2+Clp*KZ2+(Clr+Cnp)*KXZ)
+    A=16*mub**3*(KX2*KZ2-KXZ**2)
+    B=-4*mub**2*(2*CYb*(KX2*KZ2-KXZ**2)+Cnr*KX2+Clp*KZ2+(Clr+Cnp)*KXZ)
     C=2*mub*((CYb*Cnr-CYr*Cnb)*KX2+(CYb*Clp-Clb*CYp)*KZ2+((CYb*Cnp-Cnb*CYp)+(CYb*Clr-Clb*CYr))*KXZ+4*mub*Cnb*KX2+4*mub*Clb*KXZ+0.5*(Clp*Cnr-Cnp*Clr))
     D=-4*mub*CL*(Clb*KZ2+Cnb*KXZ)+2*mub*(Clb*Cnp-Cnb*Clp)+0.5*CYb*(Clr*Cnp-Cnr*Clp)+0.5*CYp*(Clb*Cnr-Cnb*Clr)+0.5*CYr*(Clp*Cnb-Cnp*Clb)
-    E=CL*(Clb*Cnr-Cnp*Clb)
+    E=CL*(Clb*Cnr-Cnb*Clr)
     eigs = sp.roots(np.array([A,B,C,D,E]))
     return eigs
