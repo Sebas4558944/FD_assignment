c48299a49da60d6df46c6d84a526e3cff1bd75ec
Martijn <ms.jorissen@tiscali.nl>
2019-03-25 11:05:10 +0100

    1 spare line

--- ss_model_2.py
+++ ss_model_2.py
@@ -99,7 +99,7 @@ reallabel='real'
 fakelabel='fake'
 modes = ["phugoid", "short period", "dutch roll","dutch roll yd", "aperiodic roll", "spiral" ]
 
-plotting=[True]*6
+plotting=[True, True, False, False, False, False]
 
 indices, times, altitudes, velocities, alphas,  pitches, rolls, yaws, ailerons, rudders, elevators = getEigenmotions()
 ######phugoid (n=0): plotting speed, altitude and angle of attack against time          =
@@ -114,7 +114,6 @@ indices, times, altitudes, velocities, alphas,  pitches, rolls, yaws, ailerons, 
 #======                     phugoid                             =======
 #======================================================================
 #Phugoid is a pulse on the elevator
-n=0
 for n in range(6):
     if plotting[n]:
         #Get Flight Data
